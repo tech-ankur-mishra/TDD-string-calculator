b852f9c80a82068c7dae7463db6cf88d7b953640
tech-ankur-mishra <tech.ankurmishra@gmail.com>
2025-03-16 21:39:52 +0530

    Test: Add test for negative number exception

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -30,4 +30,10 @@ describe("StringCalculator", () => {
   test("supports custom delimiter", () => {
     expect(calculator.add("//;\n1;2;3")).toBe(6);
   });
+
+  test("throws error for negative numbers", () => {
+    expect(() => calculator.add("1,-2,3,-4")).toThrow(
+      "negative numbers not allowed -2,-4"
+    );
+  });
 });
