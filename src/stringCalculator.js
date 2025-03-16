5ffa013407d2d33aa7f56e5774e19775c19a76e5
tech-ankur-mishra <tech.ankurmishra@gmail.com>
2025-03-16 21:19:57 +0530

    Feat: Handle single number input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,7 @@
 class StringCalculator {
   add(numbers) {
-    return numbers === "" ? 0 : null;
+    if (numbers === "") return 0;
+    return parseInt(numbers, 10);
   }
 }
-
 module.exports = StringCalculator;
