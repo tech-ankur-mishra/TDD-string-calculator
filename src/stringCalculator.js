72420ec18c2c7005c9790e4c7a4826d95840c330
tech-ankur-mishra <tech.ankurmishra@gmail.com>
2025-03-16 21:38:22 +0530

    Feat: Support custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,16 @@
 class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
+
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].slice(2));
+      numbers = parts[1];
+    }
+
     return numbers
-      .split(/,|\n/)
+      .split(delimiter)
       .map(Number)
       .reduce((sum, num) => sum + num, 0);
   }
