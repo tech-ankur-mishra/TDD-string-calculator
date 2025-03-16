a8e4ed8ec539db225210ef0182141cd71a1378e8
tech-ankur-mishra <tech.ankurmishra@gmail.com>
2025-03-16 21:46:26 +0530

    Feat: Handle negative numbers with exception

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,16 +3,22 @@ class StringCalculator {
     if (numbers === "") return 0;
 
     let delimiter = /,|\n/;
+
     if (numbers.startsWith("//")) {
       const parts = numbers.split("\n");
       delimiter = new RegExp(parts[0].slice(2));
-      numbers = parts[1];
+      numbers = parts.slice(1).join("\n");
     }
 
-    return numbers
-      .split(delimiter)
-      .map(Number)
-      .reduce((sum, num) => sum + num, 0);
+    const numArray = numbers.split(delimiter).map(Number);
+
+    const negatives = numArray.filter((n) => n < 0);
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
+
 module.exports = StringCalculator;
