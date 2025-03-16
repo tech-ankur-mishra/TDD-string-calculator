b5b1ea4ac371f77ecead092bf2e4018be0279ec6
tech-ankur-mishra <tech.ankurmishra@gmail.com>
2025-03-16 20:57:36 +0530

    Test: Add test for empty string returning 0

---
+++ test/stringCalculator.test.js
@@ -0,0 +1,13 @@
+const StringCalculator = require("./stringCalculator");
+
+describe("StringCalculator", () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test("returns 0 for an empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+});
